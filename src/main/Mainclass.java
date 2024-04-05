e241b5d027dea11f67d870de302b6eb79e18713f
hieux15 <tomhank339@gmail.com>
2024-04-05 18:00:30 +0700

    Initial commit

---
+++ src/main/Mainclass.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.Scanner;
+
+public class Mainclass {
+    public static void main(String[] args) {
+      System.out.println("dssadas");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("dssadas");
+        System.out.println("dssadas");System.out.println("dssadas");
+        System.out.println("dssadas");
+    }
+}
